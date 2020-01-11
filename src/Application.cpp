14b0787c2a4c1d615d84d0291d11a3ed937c071e
Andrew Maher <andrewaacodo12@gmail.com>
2020-01-11 02:54:45 +0200

    Reading Shaders from External File

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,48 @@
 #include<GL/glew.h>
 #include <GLFW/glfw3.h>
-#include<iostream>
 
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::fstream stream(filepath);
+
+	enum class ShaderType {
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << "\n";
+		}
+	}
+
+	return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -45,6 +86,14 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 	return program;
 }
 
+void MoveLeft(float* positions, int size , int dimension, float speed = 0.1)
+{
+	for (int i = 0; i < size; i += dimension)
+	{
+		positions[i] += speed;
+	}
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -84,27 +133,9 @@ int main(void)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
@@ -115,6 +146,7 @@ int main(void)
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
+		//MoveLeft(positions, 6, 2);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -122,6 +154,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+	
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
